27414e5f62ef4fc5b920eeb20769532636df6376
tabrasa1 <en.mailsandsnails@gmail.com>
2025-09-17 10:55:48 +1000

    ouchy

---
+++ src/main/java/TestDB.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+public class TestDB {
+public static void main(String[] args) {
+try {
+Connection conn = DriverManager.getConnection(
+"jdbc:mysql://localhost:3306/wiki_db", "root", "password"
+);
+System.out.println("Connected!");
+conn.close();
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+}
