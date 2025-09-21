83b646d2266bc92af081be4c407f62cb47147b49
tabrasa1 <en.mailsandsnails@gmail.com>
2025-09-21 23:16:04 +1000

    from nothing..

--- src/main/java/TestDB.java
+++ src/main/java/TestDB.java
@@ -1,14 +1,13 @@
 import java.sql.*;
 public class TestDB {
-public static void main(String[] args) {
-try {
-Connection conn = DriverManager.getConnection(
-"jdbc:mysql://localhost:3306/wiki_db", "root", "password"
-);
-System.out.println("Connected!");
-conn.close();
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/wiki_db", "root", "password");
+            System.out.println("Connected!");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
